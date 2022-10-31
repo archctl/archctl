88701a63edcc05615c6d7a78a9625bd2e65c8301
Adrián García <adrian.garcia6@decathlon.com>
2022-10-31 13:03:16 +0100

    ARCH-21 First CI Workflow and First Test

---
+++ tests/test_cli.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+import pytest
+
+from click.testing import CliRunner
+
+from archctl.__main__ import main
+
+
+@pytest.fixture(scope='session')
+def cli_runner():
+    """Fixture that returns a helper function to run the cookiecutter cli."""
+    runner = CliRunner()
+
+    def cli_main(*cli_args, **cli_kwargs):
+        """Run cookiecutter cli main with the given args."""
+        return runner.invoke(main, cli_args, **cli_kwargs)
+
+    return cli_main
+    
+
+@pytest.fixture(params=['-V', '--version'])
+def version_cli_flag(request):
+    """Pytest fixture return both version invocation options."""
+    return request.param
+
+
+def test_cli_version(cli_runner, version_cli_flag):
+    """Verify Archctl version output by `archctl` on cli invocation."""
+    runner = CliRunner()
+
+    result = cli_runner(version_cli_flag)
+    assert result.exit_code == 0
+    assert result.output.startswith('Archctl')
